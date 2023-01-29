dd607b05ad288fb30d2d0d89004a7f192b727931
AfghanAcademy <afghanacademy2@gmail.com>
2023-01-29 23:25:39 +0500

    new changes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // return view('welcome');
 
-    return "hello";
+    // return "hello";
+
+    return "Hello World";
 });
